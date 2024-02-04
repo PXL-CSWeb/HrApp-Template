9dc5f9297f8d1ff35f421a7a42841ae5aa9e80cf
Wim Bervoets <wim.bervoets@pxl.be>
2024-02-04 20:32:10 +0100

    - Added TagHelper login-type - Added assets folder for readme.md - Updated readme.md

--- HrApp/TagHelpers/HyperlinkTaghelper.cs
+++ HrApp/TagHelpers/HyperlinkTaghelper.cs
@@ -2,13 +2,45 @@
 
 namespace HrApp.TagHelpers
 {
-    [HtmlTargetElement("a", Attributes = "background-color")]
+    [HtmlTargetElement("a", Attributes = "login-type")]
     public class HyperlinkTagHelper : TagHelper
     {
-        public string BackgroundColor { get; set; } = "danger";
+        public LoginType LoginType { get; set; } = LoginType.Username;
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            output.Attributes.SetAttribute("class", $"btn btn-{BackgroundColor}");
+            switch(LoginType)
+            {
+                case LoginType.Username:
+                    output.Content.SetContent("<i class=\"fa-solid fa-user\"></i>"
+                                            + "<h4>Username</h4>");
+                    break;
+                case LoginType.Email:
+                    output.Content.SetContent("<i class=\"fa-solid fa-at\"></i>"
+                                            + "<h4>Email</h4>");
+                    break;
+                case LoginType.Facebook:
+                    output.Content.SetContent("<i class=\"fa-brands fa-facebook\"></i>"
+                                            + "<h4>Facebook</h4>");
+                    break;
+                case LoginType.Google:
+                    output.Content.SetContent("<i class=\"fa-brands fa-google\"></i>"
+                                            + "<h4>Google</h4>");
+                    break;
+                case LoginType.External:
+                    output.Content.SetContent("<i class=\"fa-solid fa-fingerprint\"></i>"
+                                            + "<h4>Open Id</h4>");
+                    break;
+            }
+
         }
+    }
+
+    public enum LoginType
+    {
+        Username,
+        Email,
+        Facebook,
+        Google,
+        External
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# HrApp - Template
+# HrApp
+
+- Vervolledig de AccountController zodat gebruikers kunnen aanmelden met:
+  - Username: gebruikersnaam en wachtwoord
+  - Email: emailadres en wachtwoord
+
+![Login](assets/login.png)
